a2b2033d243c29560a53a396965c0644ea8afbef
Mateusz <mateusz@dcx.bz>
2016-01-21 12:23:27 +0100

    init

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// ractive-events-datetimepicker tests
+// =============================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
